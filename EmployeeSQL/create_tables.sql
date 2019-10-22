8c3e5a7c97c6d3c86e9683b69b4c7f5c8ebfbded
Brian M Walsh <bwalsh@Brian-Walshs-Computer-2.local>
2019-10-22 09:58:21 -0700

    create tables

---
+++ EmployeeSQL/create_tables.sql
@@ -0,0 +1,57 @@
+CREATE TABLE departments (
+	dept_no VARCHAR PRIMARY KEY,
+	dept_name VARCHAR
+);
+
+SELECT * FROM departments;
+
+CREATE TABLE employees (
+	emp_no INT PRIMARY KEY,
+	birth_date DATE,
+	first_name VARCHAR,
+	last_name VARCHAR,
+	gender VARCHAR,
+	hire_date DATE
+);
+
+SELECT * FROM employees;
+
+CREATE TABLE salaries (
+	emp_no INT PRIMARY KEY,
+	salary INT,
+	from_date DATE,
+	to_date DATE
+);
+
+SELECT * FROM salaries;
+
+CREATE TABLE titles (
+	emp_no INT,
+	title VARCHAR,
+	from_date DATE,
+	to_date VARCHAR,
+	FOREIGN KEY (emp_no) REFERENCES employees(emp_no)
+);
+
+SELECT * FROM titles;
+
+CREATE TABLE dept_emp (
+	emp_no INT,
+	dept_no VARCHAR,
+	from_date DATE,
+	to_date VARCHAR,
+	FOREIGN KEY (emp_no) REFERENCES employees(emp_no),
+	FOREIGN KEY (dept_no) REFERENCES departments(dept_no)
+);
+
+SELECT * FROM dept_emp;
+
+CREATE TABLE dept_manager (
+	dept_no VARCHAR,
+	emp_no INT PRIMARY KEY,
+	from_date DATE,
+	to_date VARCHAR,
+	FOREIGN KEY (dept_no) REFERENCES departments(dept_no)
+);
+
+SELECT * FROM dept_manager;
